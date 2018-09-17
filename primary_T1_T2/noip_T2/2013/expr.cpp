5c9ed8d71dab12d1ace67d8f877f95f15c48c5ee
spermakert <spermakert@163.com>
2018-09-17 15:51:47 +0800

    change

--- primary_T1_T2/noip_T2/2013/expr.cpp
+++ primary_T1_T2/noip_T2/2013/expr.cpp
@@ -1,55 +1,59 @@
 /*************************************************************************
-    > File Name: mine.cpp
+    > File Name: expr.cpp
     > Author: spermakert
-    > Mail: spermakert@163.com
-    > Created Time: Fri 26 Aug 2018 10:12:26 PM CST
+    > Mail: spermakert@163.com 
+    > Created Time: Mon 17 Sep 2018 03:23:43 PM CST
  ************************************************************************/
 
 #include<iostream>
 #include<string>
-
+#include<vector>
 using namespace std;
-
-
-
 int main()
 {
+	freopen("expr.in","r",stdin);
+	freopen("expr.out","w",stdout);
 
-    freopen ("expr.in","r",stdin);
-    //freopen ("librarian.out","w",stdout);
-    string str;
-    cin>>str;
-    int strlen = str.size();//str.length();
-    int i=0,num_tmp=0,j=0,k=0,cnum_tmp,ans=0;
-    while(i<strlen)
-    {
-        j=i;
-        cnum_tmp=0;
-        num_tmp=0;
-        while(j<strlen && str[j]>='0' && str[j]<='9')
-        {
-            num_tmp=num_tmp*10+(str[j]-'0');
-            j++;
-        }
-        cnum_tmp = num_tmp;
-        while(j<strlen && str[j]=='*')
-        {
-            k=j+1;
-            num_tmp=0;
-            while(k<strlen && str[k]>='0' && str[k]<='9')
-            {
-                num_tmp= num_tmp=num_tmp*10+(str[k]-'0');
-                k++;
-            }
-            cnum_tmp = cnum_tmp * num_tmp;
-            j=k;
-        }
-        i=j+1;
-        ans = ans +cnum_tmp;
-    }
+	string expr_str;
+	getline(cin,expr_str);
 
-    cout<<ans%10000;
-    return 0;
+	vector<char> symbol;
+	vector<int> num;
+
+	//1+2*4*6+6+7
+	//
+	int len = expr_str.size();
+	int tmp_num = 0;
+	for(int i=0;i<len;i++)
+	{
+		if(expr_str[i]=='*' || expr_str[i]=='+')
+		{
+			num.push_back(tmp_num);
+			symbol.push_back(expr_str[i]);
+			tmp_num = 0;
+		}
+		else
+			tmp_num = tmp_num * 10 + (expr_str[i] - '0') % 10;
+	}
+	num.push_back(tmp_num);
+
+	int ans = 0;
+	int symbol_len = symbol.size();
+	for(int i=0;i<symbol_len;i++)
+	{
+		if(symbol[i]=='*')
+		{
+			num[i+1] = num[i] * num[i+1];
+			num[i] = 0;
+		}
+	}
+	int num_len = num.size();
+	for(int i=0;i<num_len;i++)
+	{
+		ans = ans + num[i];
+	}
+
+	cout<<ans<<endl;
+
+	return 0;
 }
-
-
